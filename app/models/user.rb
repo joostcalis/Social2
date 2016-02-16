2887e1792232db8d032843afb9114ecd1d4ac71a
joostcalis <joost_calis@hotmail.com>
2016-02-16 15:56:43 +0100

    postmodel set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ has_and_belongs_to_many :friends, join_table: :friends_users
+ has_many :outgoing_friend_requests, class_name: FriendRequest
+ has_many :incoming_friend_requests, class_name: FriendRequest, foreign_key: :friend_id
+
+         def to_friend
+           Friend.find(id)
+         end
 end
--- db/migrate/20160216143943_create_join_table.rb
+++ db/migrate/20160216143943_create_join_table.rb
@@ -1,6 +1,6 @@
 class CreateJoinTable < ActiveRecord::Migration
   def change
-    create_join_table :, : do |t|
+    create_join_table :friends, :users do |t|
        t.index [:friend_id, :user_id]
        t.index [:user_id, :friend_id]
     end
