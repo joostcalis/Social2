dde5d6db5785fe48f73a71eb1271b2b5c61aeeba
Chiel Hackman <chielhackman@gmail.com>
2016-02-16 17:51:01 +0100

    Defined show methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  def show
+    @posts = Post.where(:user_id => current_user.friendships)
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:post_id, :content, :image, :user_id)
+    end
 end
